44ef20472af6e0b8b13cb65b5b5bdd88167cc175
Ashish Meena <ashishmeena9785110509@gmail.com>
2026-09-19 14:46:42 +0530

    backend with fastapi

--- server/src/routes/me.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { myStats } from '../controllers/statsController.js';
-import { requireAuth } from '../middleware/auth.js';
-
-const router = Router();
-
-router.get('/stats', requireAuth, myStats);
-
-export default router;
